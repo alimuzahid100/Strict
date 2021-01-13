e2090115905a6d6d8c6475d92811c2d2fa3499e1
alimuzahid100 <58989778+alimuzahid100@users.noreply.github.com>
2021-01-13 11:33:45 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,58 @@
+// wow.js plug in code start
+new WOW().init();
+// wow.js plug in code end
+
+// wow.js plug in code start
+
+var typed3 = new Typed('.simple', {
+    strings: ['& pure design'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    smartBackspace: true, // this is a default
+    loop: true
+  });
+
+  var typed3 = new Typed('.typeport', {
+    strings: ['your work like a pro.'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    smartBackspace: true, // this is a default
+    loop: true
+  });
+
+  var typed3 = new Typed('.stay', {
+    strings: ['with us'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    smartBackspace: true, // this is a default
+    loop: true
+  });
+// wow.js plug in code end
+
+// justflip.js plugin code start
+
+  $('.img-flip').justFlipIt({
+  });
+
+// justflip.js plugin code end
+
+// magnifik.min.js plugin code start
+
+  $('.example').magnifik({
+    ratio: 0.5
+  });
+
+
+// magnifik.min.js plugin code end
+
+// stickup.min.js plugin code start
+
+jQuery(function($) {
+  $(document).ready( function() {
+    //enabling stickUp on the '.navbar-wrapper' class
+    $('.navbar-wrapper').stickUp();
+  });
+});
+
+// stickup.min.js plugin code end
+  
